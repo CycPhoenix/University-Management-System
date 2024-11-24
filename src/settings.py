415f877e8a0860d97a7c2106483d74f563785eb8
CycPhoenix <63090086+CycPhoenix@users.noreply.github.com>
2024-11-25 00:20:58 +0800

    Create settings.py

---
+++ src/settings.py
@@ -0,0 +1,16 @@
+# settings.py
+# File paths for various data files in the system
+ATTENDANCE_FILE = 'data/attendance.txt'
+COURSES_FILE = 'data/courses.txt'
+ENROLLMENTS_FILE = 'data/enrollments.txt'
+FEES_FILE = 'data/fees.txt'
+GRADES_FILE = 'data/grades.txt'
+LECTURERS_FILE = 'data/lecturers.txt'
+LECTURER_COURSES_FILE = 'data/lecturer_courses.txt'
+STUDENTS_FILE = 'data/students.txt'
+
+DEPARTMENTS_FILE = 'data/departments.txt'
+FOUNDATION_FILE = 'data/courses/foundation.txt'
+DIPLOMA_FILE = 'data/courses/diploma.txt'
+UNDERGRADUATE_DIR = 'data/courses/undergraduate/'
+POSTGRADUATE_DIR = 'data/courses/postgraduate/'
