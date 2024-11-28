89bde7620b1920f1c7ea44e9a91d6dbffa1dfc64
CycPhoenix <63090086+CycPhoenix@users.noreply.github.com>
2024-11-28 20:54:00 +0800

    Removed __init__.py

--- src/admin/courses/__init__.py
+++
@@ -1,5 +0,0 @@
-from .add_course import add_course
-from .remove_course import remove_course
-from .update_course import update_course
-from .view_all_courses import view_all_courses
-from .manage_courses import manage_courses
--- src/utils/__init__.py
+++
@@ -1,3 +0,0 @@
-from .display_choices import display_choices
-from .load_data import load_data
-from .write_data import write_data
