3d8e22f2213ab551443c01bb96105dd01b122feb
CycPhoenix <63090086+CycPhoenix@users.noreply.github.com>
2024-11-28 20:59:02 +0800

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,26 +1,54 @@
-# from src import lecturer_functions, student_functions, registrar_functions, accountant_functions
 from admin.admin_menu import admin_menu
-
+from utils.display_choices import display_choices
 
 def main_menu():
+    """Main menu to select system role."""
     while True:
         print("\n--- University Management System ---")
-        print("1. Administrator")
-        print("2. Lecturer")
-        print("3. Student")
-        print("4. Registrar")
-        print("5. Accountant")
-        print("6. Exit")
-        print("------------------------------------")
-        choice = input("\nSelect an option: ").strip()
+        ascii_art = r"""
+                              _____                    _____                    _____          
+                             /\    \                  /\    \                  /\    \         
+                            /::\____\                /::\____\                /::\    \        
+                           /:::/    /               /::::|   |               /::::\    \       
+                          /:::/    /               /:::::|   |              /::::::\    \      
+                         /:::/    /               /::::::|   |             /:::/\:::\    \     
+                        /:::/    /               /:::/|::|   |            /:::/__\:::\    \    
+                       /:::/    /               /:::/ |::|   |            \:::\   \:::\    \   
+                      /:::/    /      _____    /:::/  |::|___|______    ___\:::\   \:::\    \  
+                     /:::/____/      /\    \  /:::/   |::::::::\    \  /\   \:::\   \:::\    \ 
+                    |:::|    /      /::\____\/:::/    |:::::::::\____\/::\   \:::\   \:::\____\
+                    |:::|____\     /:::/    /\::/    / ~~~~~/:::/    /\:::\   \:::\   \::/    /
+                     \:::\    \   /:::/    /  \/____/      /:::/    /  \:::\   \:::\   \/____/ 
+                      \:::\    \ /:::/    /               /:::/    /    \:::\   \:::\    \     
+                       \:::\    /:::/    /               /:::/    /      \:::\   \:::\____\    
+                        \:::\__/:::/    /               /:::/    /        \:::\  /:::/    /    
+                         \::::::::/    /               /:::/    /          \:::\/:::/    /     
+                          \::::::/    /               /:::/    /            \::::::/    /      
+                           \::::/    /               /:::/    /              \::::/    /       
+                            \::/____/                \::/    /                \::/    /        
+                             ~~                       \/____/                  \/____/         
+        """
         
+        print(ascii_art)
+
+        options = {
+            '1': 'Administrator',
+            '2': 'Lecturer',
+            '3': 'Student',
+            '4': 'Registrar',
+            '5': 'Accountant',
+            '6': 'Exit System'
+        }
+        choice = display_choices(options)
+
         if choice == '1':
             admin_menu()
         elif choice == '6':
-            print("\nExiting the system.")
+            print("Exiting the system. Goodbye!")
             break
         else:
             print("Invalid choice. Please try again.")
-        
+
+
 if __name__ == "__main__":
     main_menu()
