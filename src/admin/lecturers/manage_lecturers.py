2681ed5dd10ff568d8df120f036be3229aae04f9
CycPhoenix <63090086+CycPhoenix@users.noreply.github.com>
2024-12-04 17:24:11 +0800

    Update manage_lecturers.py

--- src/admin/lecturers/manage_lecturers.py
+++ src/admin/lecturers/manage_lecturers.py
@@ -5,16 +5,29 @@ from admin.lecturers.view_all_lecturers import view_all_lecturers
 
 def manage_lecturers():
     """Menu to manage lecturers."""
+    ml_art = r"""
+     __ __                            _               _                              
+    |  \  \ ___ ._ _  ___  ___  ___  | |   ___  ___ _| |_ _ _  _ _  ___  _ _  ___
+    |     |<_> || ' |<_> |/ . |/ ._> | |_ / ._>/ | ' | | | | || '_>/ ._>| '_><_-<
+    |_|_|_|<___||_|_|<___|\_. |\___. |___|\___.\_|_. |_| `___||_|  \___.|_|  /__/
+                      <___'                                                  
+    """
+    separator_length = max(len(line) for line in ml_art.splitlines())
+    separator = "=" * separator_length
+
     while True:
-        print("\n--- Manage Lecturers ---")
-        options = {
-            '1': 'Add Lecturer',
-            '2': 'Remove Lecturer',
-            '3': 'Update Lecturer',
-            '4': 'View All Lecturers',
-            '5': 'Back to Admin Menu'
-        }
-        choice = display_choices(options)
+        print()
+        print(separator)
+        print(ml_art)
+        print(separator)
+        print("\nSelect an option:")
+        print("1. Add Lecturer")
+        print("2. Remove Lecturer")
+        print("3. Update Lecturer")
+        print("4. View All Lecturers")
+        print("5. Back to Admin Menu")
+
+        choice = input("\nEnter your choice: ").strip()
 
         if choice == '1':
             try:
